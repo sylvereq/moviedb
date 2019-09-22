d4c8791eb091d718781e69a3787d4c70ad10367b
Edgar Österle <edgar.oesterle@gmail.com>
2019-09-22 19:10:34 +0200

    first one :)

---
+++ app/src/main/java/android/oesterle/com/moviedb/MainActivity.java
@@ -0,0 +1,25 @@
+package android.oesterle.com.moviedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
